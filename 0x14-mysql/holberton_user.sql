ac58b04a1c166e7cdcda0ffd168bed0810dd1713
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-04-18 21:08:12 +0100

    Update holberton_user.sql

--- 0x14-mysql/holberton_user.sql
+++ 0x14-mysql/holberton_user.sql
@@ -1,1 +1,4 @@
-qwerty
+-- making a user for holberton
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+SHOW GRANTS FOR 'holberton_user'@'localhost';
