354a37c23257a195547c5093d72e5e820b471066
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-04-18 21:07:24 +0100

    Update Tyrell_corp.sql

--- 0x14-mysql/Tyrell_corp.sql
+++ 0x14-mysql/Tyrell_corp.sql
@@ -1,1 +1,7 @@
-qwertyu
+-- for the first project
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+use tyrell_corp;
+CREATE TABLE IF NOT EXISTS nexus6(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+INSERT INTO `nexus6` (`id`, `name`) VALUES ("1", "Leon");
+select * from nexus6;
+GRANT SELECT ON `tyrell_corp`.* TO 'holberton_user'@'localhost';
